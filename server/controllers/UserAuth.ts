79903f13aca3aeff2d47873e0868995b71056658
kuldeep-singh111 <ks6201709@gmail.com>
2025-07-26 18:17:34 +0530

    same site

--- server/controllers/UserAuth.ts
+++ server/controllers/UserAuth.ts
@@ -51,6 +51,7 @@ export const login = async (req: Request, res: Response) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
+            sameSite: 'none',
             maxAge: 60 * 60 * 1000,
         });
 
