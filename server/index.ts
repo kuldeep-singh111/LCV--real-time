adc763e2c9d559bc12d07b6bc824e2f8c2f8f7fd
kuldeep-singh111 <ks6201709@gmail.com>
2025-07-26 16:55:19 +0530

    b DOne

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 // const io = new Server(server);
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://lcv-real-time-96hk.vercel.app',
     credentials: true
 }));
 
